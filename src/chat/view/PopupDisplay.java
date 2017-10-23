69df359e9c5602add1c75d3cb3c8d0bf3e718929
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-23 13:44:09 -0600

    Added a length checker, added tests, changed popupdisplay, and edited runner

--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -1,21 +1,33 @@
-package View;
+package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 /**
  * Simple GUI class using JOptionPane.
  * @author Dustin Schimel
  * @version 1.0 03.10.2017
  */
+
 public class PopupDisplay
 {
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay()
+	{
+		icon = new ImageIcon(getClass().getResource("images/chatbot.png"));
+		windowTitle = "Chatbot says";
+	}
+	
 	/**
 	 * Displays a popup showing a supplied String to the user.
 	 * @param textToDisplay The text to be displayed.
 	 */
-	public void displayText(String textToDisplay)
+	
+	public void displayText(String message)
 	{
-		JOptionPane.showMessageDialog(null, textToDisplay);
+		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
 	}
 	
 	/**
@@ -23,11 +35,12 @@ public class PopupDisplay
 	 * @param questionToDisplay The question being asked.
 	 * @return The users response to the question.
 	 */
-	public String getResponse(String questionToDisplay)
+	
+	public String collectResponse(String question)
 	{
 		String answer = "";
 		
-		answer += JOptionPane.showInputDialog(null, questionToDisplay);
+		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null, "");
 		
 		return answer;
 	}
