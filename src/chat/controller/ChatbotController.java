cc48472dd09d9c59ef885c62ecdb8d0d32162dc2
Dustin Schimel <dustin.schimel@gmail.com>
2017-11-03 13:22:41 -0600

    Added helpful comments to the controller

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -16,21 +16,21 @@ public class ChatbotController
 	
 	public void start()
 	{
-		String response = display.collectResponse("What do you want to talk about?");
+		String response = display.collectResponse("What do you want to talk about?");	//Prompts the user for a response
 		
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
+		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))		//Checks for if the user's response is greater than or equal to three, not null,
+		{																			//and that it's not the word 'quit'
+			response = popupChat(response);											//Calls popupChat and gives it response
+			response = display.collectResponse(response);								//Displays the users response, bot statement and question, the user can then answer and it will loop
 		}
 	}
 	
 	private String popupChat(String chat)
 	{
-		String chatbotSays = "";
+		String chatbotSays = "";														//Creates a new variable with the value ""
 		
-		chatbotSays += chatbot.processConversation(chat);
+		chatbotSays += chatbot.processConversation(chat);								//Calls processConversation and gives it chat, the returned value is then given to chatbotSays
 		
-		return chatbotSays;
+		return chatbotSays;															//returns the value to response in start
 	}
 }
