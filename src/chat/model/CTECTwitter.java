0ebb7d7b54a147a195472789fb4d22f680256b29
Dustin Schimel <dustin.schimel@gmail.com>
2018-03-01 11:50:53 -0700

    Initialized needed components in constructor

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -23,12 +23,17 @@ public class CTECTwitter
 {
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweets;
+	private List<String> tweetedWords;
+	private long totalWordCount;
 	
 	public CTECTwitter(ChatbotController appController)
 	{
 		this.appController = appController;
 		this.chatbotTwitter = TwitterFactory.getSingleton();
-		
+		this.searchedTweets = new ArrayList<Status>();
+		this.tweetedWords = new ArrayList<String>();
+		this.totalWordCount = 0;
 	}
 	
 	public void sendTweet(String textToTweet)
